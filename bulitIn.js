0b9dae254d5bc8bbf4330978ef869d1259f1f8a9
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2025-11-08 20:13:34 +0530

    constellations working

--- bulitIn.js
+++ bulitIn.js
@@ -1,11 +1,3 @@
-
-// 2. Stargazing Log
-// A stargazing club logs visible constellations from each night:
-// ["Orion", "Leo"]
-// ["Taurus"]
-// ["Orion", "Gemini"]
-// Combine everyone’s observations into one list of all constellations spotted.
-
 const count = function (value, color) {
   return color === 'blue' ? value + 1 : value;
 }
@@ -14,18 +6,19 @@ const RibbonCount = function(array) {
   return array.reduce(count, 0);
 }
 
-// const constellations = [["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]];
-// console.log(constellations.flat());
+const constellations = function (array) {
+  return array.flat();
+}
 
-// console.log(ribbonCount.reduce(count, 0));
 const isArray = function (array) {
   return typeof array === 'object';
 }
 
 function areEqual(array1, array2) {
-  if(array1.length === array2.length) {
+  if(array1.length !== array2.length) {
     return false;
   }
+
   for (let index = 0; index < array1.length; index++) {
     if(!areDeepEqual(array1[index], array2[index])) {
       return false;
@@ -65,11 +58,12 @@ function testRibbonCount() {
 }
 
 function testConstellations() {
-  console.log(testFunctionality('blue ribbon count is 2', ["red", "blue", "red", "green", "red", "blue"], RibbonCount,2));
+  console.log(testFunctionality('blue ribbon count is 2', [["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]], constellations,[ "Orion", "Leo", "Taurus", "Orion", "Gemini" ]));
 }
 
 function testAll() {
-  testRibbonCount();
+  // testRibbonCount();
+  testConstellations();
 }
 
 testAll();
